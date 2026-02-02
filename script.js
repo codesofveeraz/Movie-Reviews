0ff29062a561b3ff118a80d42c3990a5fa8d0cce
Biraj Pradhan <ByPsoftware@gmail.com>
2026-02-02 14:13:55 +0545

    Remove legacy script and enable Firestore-driven movies

--- script.js
+++
@@ -1,9 +0,0 @@
-import { getMovies } from "./js/movies.js";
-import { renderMovies } from "./js/ui.js";
-
-async function loadMovies() {
-  const movies = await getMovies();
-  renderMovies(movies);
-}
-
-window.addEventListener("DOMContentLoaded", loadMovies);
