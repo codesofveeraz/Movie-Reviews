3d76911359bb6097ba428f0cb5b4a7173a78c7e5
Biraj Pradhan <ByPsoftware@gmail.com>
2026-02-04 19:51:19 +0545

    gpt changes on  full

--- js/movies.js
+++ js/movies.js
@@ -1,15 +1,16 @@
 import { db } from "./firebase.js";
-import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import {
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// reference to movies collection
 const moviesRef = collection(db, "movies");
 
-// add a movie
 export async function addMovie(movie) {
   await addDoc(moviesRef, movie);
 }
 
-// get all movies
 export async function getMovies() {
   const snapshot = await getDocs(moviesRef);
   return snapshot.docs.map(doc => ({
@@ -17,3 +18,4 @@ export async function getMovies() {
     ...doc.data()
   }));
 }
+console.log("Firestore DB:", db);
