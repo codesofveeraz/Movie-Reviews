b30ef8e79e5b3d9514d5d71d9ae55055309f446c
Biraj Pradhan <ByPsoftware@gmail.com>
2026-02-04 19:43:56 +0545

    major changes

--- js/movies.js
+++ js/movies.js
@@ -1,26 +1,19 @@
 import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-export async function loadMovies() {
-  console.log("Loading movies...");
-  const movieList = document.getElementById("movie-list");
-  movieList.innerHTML = "";
+// reference to movies collection
+const moviesRef = collection(db, "movies");
 
-  const snapshot = await getDocs(collection(db, "movies"));
-
-  snapshot.forEach(doc => {
-    const movie = doc.data();
-
-    const card = document.createElement("div");
-    card.className = "movie-card";
-
-    card.innerHTML = `
-      <img src="${movie.poster}" />
-      <h3>${movie.title} (${movie.year})</h3>
-      <p>${movie.language}</p>
-    `;
-
-    movieList.appendChild(card);
-  });
+// add a movie
+export async function addMovie(movie) {
+  await addDoc(moviesRef, movie);
 }
-loadMovies();
+
+// get all movies
+export async function getMovies() {
+  const snapshot = await getDocs(moviesRef);
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  }));
+}
--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,14 @@
-alert("UI JS IS RUNNING");
+export function renderMovies(movies) {
+  const container = document.getElementById("movies-list");
+  container.innerHTML = "";
+
+  movies.forEach(movie => {
+    container.innerHTML += `
+      <div class="movie-card">
+        <img src="${movie.poster}" alt="${movie.title}" />
+        <h3>${movie.title}</h3>
+        <p>${movie.description}</p>
+      </div>
+    `;
+  });
+}
