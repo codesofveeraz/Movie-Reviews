8b1f9ba4e5711dee90695dab81aa5c0d688b4c6a
Biraj Pradhan <ByPsoftware@gmail.com>
2026-02-04 19:42:54 +0545

    Add movie functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
-console.log("MAIN JS LOADED");
+import { addMovie, getMovies } from "./movies.js";
+import { renderMovies } from "./ui.js";
 
-import { loadMovies } from "./movies.js";
-import { loadReviews } from "./reviews.js";
+const form = document.getElementById("movie-form");
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM READY");
-  loadMovies();
-  loadReviews();
-});
+if (form) {
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const movie = {
+      title: document.getElementById("title").value,
+      poster: document.getElementById("poster").value,
+      description: document.getElementById("description").value,
+      createdAt: new Date()
+    };
+
+    await addMovie(movie);
+    alert("Movie added!");
+    form.reset();
+  });
+}
+async function loadMovies() {
+  const movies = await getMovies();
+  renderMovies(movies);
+}
+
+loadMovies();
