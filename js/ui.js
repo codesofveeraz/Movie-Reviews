e1ceeea27cbebf0f02c562ef0864bb4da28e6832
Biraj Pradhan <ByPsoftware@gmail.com>
2026-02-04 19:56:56 +0545

    ui placeholder

--- js/ui.js
+++ js/ui.js
@@ -1,13 +1,23 @@
 export function renderMovies(movies) {
   const container = document.getElementById("movie-list");
+  if (!container) return;
+
   container.innerHTML = "";
 
   movies.forEach(movie => {
+    const poster = movie.poster && movie.poster.trim() !== ""
+      ? movie.poster
+      : "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg";
+
     container.innerHTML += `
       <div class="movie-card">
-        <img src="${movie.poster}" alt="${movie.title}" />
+        <img 
+          src="${poster}" 
+          alt="${movie.title}"
+          onerror="this.src='https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg'"
+        >
         <h3>${movie.title}</h3>
-        <p>${movie.description}</p>
+        <p>${movie.description || "No description yet."}</p>
       </div>
     `;
   });
